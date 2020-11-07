ef32d357a561567130f7f107ecad3ec1626ac125
8bitago <eightbitago@gmail.com>
2020-11-07 23:50:22 +0300

    Added GlobalStyles component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import theme from './src/themes/theme';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body, html {
+    font-family: ${props => props.theme.fonts.main};
+    height: 100%;
+    background-color: ${props => props.theme.colors.light1};
+  }
+`;
 
 export const wrapRootElements = ({ element }) => (
   <ThemeProvider theme={theme}>
+    <GlobalStyles />
     { element }
   </ThemeProvider>
 );
